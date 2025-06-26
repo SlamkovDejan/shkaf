429bc0fa0dd528d464809c361c29997354bc89d4
SlamkovDejan <slamkovDejan@hotmail.com>
2025-06-26 21:17:15 +0200

    add user with loaded closet dependency

--- backend/src/shkaf/auth/main.py
+++ backend/src/shkaf/auth/main.py
@@ -1,11 +1,12 @@
 import uuid
-from typing import Annotated
+from typing import Annotated, AsyncGenerator
 
 from fastapi import Depends
 from fastapi_users import FastAPIUsers
 
 from shkaf.auth.backend import auth_backend
 from shkaf.auth.user_manager import get_user_manager
+from shkaf.db import SessionDep
 from shkaf.models import User
 
 fastapi_users = FastAPIUsers[User, uuid.UUID](
@@ -14,4 +15,15 @@ fastapi_users = FastAPIUsers[User, uuid.UUID](
 )
 
 current_active_user = fastapi_users.current_user(active=True)
+
 CurrentUserDep = Annotated[User, Depends(current_active_user)]
+
+
+async def get_current_active_user_with_closet(
+    session: SessionDep, user: CurrentUserDep
+) -> AsyncGenerator[User, None]:
+    await session.refresh(user, attribute_names=["closet"])
+    yield user
+
+
+UserWithClosetDep = Annotated[User, Depends(get_current_active_user_with_closet)]
